45d44fcb688fc2649c8c79a07910a1ab2746bd1f
Sarthak <sarthakrawatbiz@gmail.com>
2024-07-10 14:10:17 +0530

    Added mobile hamburger menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Navbar from "./components/Navbar"
-
-import Home from "./pages/Home"
 import Footer from "./components/Footer"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Blog from "./pages/Blog"
 
 function App() {
     const name = `                                                                                                 
@@ -30,6 +31,8 @@ EEEEEEEEEEEEEEEEEEEEEEiiiiiiiikkkkkkkk    kkkkkkk
             <Navbar />
             <Routes>
                 <Route index element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="blogs" element={<Blog />} />
             </Routes>
             <Footer />
         </BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import { Menu, X } from "lucide-react"
 import Copy from "./Copy"
 import styles from "./Navbar.module.css"
 
 function Navbar() {
     const [scrolled, setScrolled] = useState(false)
-    // const [time, setTime] = useState(
-    //     new Date().toLocaleTimeString("en-US", { hour12: false }),
-    // )
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     useEffect(() => {
         const handleScroll = () => {
@@ -23,36 +22,77 @@ function Navbar() {
         }
     }, [])
 
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         setTime(new Date().toLocaleTimeString("en-US", { hour12: false }))
-    //     }, 1000)
-    //     return () => {
-    //         clearInterval(interval)
-    //     }
-    // }, [])
+    useEffect(() => {
+        if (mobileMenuOpen) {
+            document.body.style.overflow = "hidden"
+        } else {
+            document.body.style.overflow = "unset"
+        }
+    }, [mobileMenuOpen])
+
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!mobileMenuOpen)
+    }
 
     return (
-        <nav
-            className={`${styles.navbar} ${scrolled ? styles.scrolled : styles.transparent}`}
-        >
-            <div>
-                <Link to="/" className={styles.logo}>
-                    <h1>
-                        Eik {/*<span className={styles.time}>[{time}]</span> */}
-                    </h1>
-                </Link>
+        <>
+            <nav
+                className={`${styles.navbar} ${
+                    scrolled ? styles.scrolled : styles.transparent
+                }`}
+            >
+                <div>
+                    <Link to="/" className={styles.logo}>
+                        <h1>Eik</h1>
+                    </Link>
+                </div>
+                <div className={styles.desktopMenu}>
+                    <Link to="/about" className={styles.navLink}>
+                        About
+                    </Link>
+                    <Link to="/blogs" className={styles.navLink}>
+                        Blogs
+                    </Link>
+                    <Copy />
+                </div>
+                <div className={styles.mobileMenuToggle}>
+                    <button onClick={toggleMobileMenu}>
+                        <Menu size={24} />
+                    </button>
+                </div>
+            </nav>
+            <div
+                className={`${styles.mobileMenu} ${mobileMenuOpen ? styles.open : ""}`}
+            >
+                <div className={styles.mobileMenuContent}>
+                    <button
+                        className={styles.closeButton}
+                        onClick={toggleMobileMenu}
+                    >
+                        <X size={24} />
+                    </button>
+                    <div className={styles.mobileMenuLinks}>
+                        <Link
+                            to="/about"
+                            className={styles.mobileNavLink}
+                            onClick={toggleMobileMenu}
+                        >
+                            About
+                        </Link>
+                        <Link
+                            to="/blogs"
+                            className={styles.mobileNavLink}
+                            onClick={toggleMobileMenu}
+                        >
+                            Blogs
+                        </Link>
+                        <div className={styles.mobileCopy}>
+                            <Copy />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className={styles.navLinks}>
-                <Link to="/about" className={styles.navLink}>
-                    About
-                </Link>
-                <Link to="/blogs" className={styles.navLink}>
-                    Blogs
-                </Link>
-                <Copy />
-            </div>
-        </nav>
+        </>
     )
 }
 
