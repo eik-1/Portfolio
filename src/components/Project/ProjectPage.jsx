dfe36591b0f1c01494fa35d269607b7815bd2278
Sarthak <sarthakrawatbiz@gmail.com>
2024-09-06 17:37:52 +0530

    Added ProjectPage component

--- src/components/Project/ProjectPage.jsx
+++ src/components/Project/ProjectPage.jsx
@@ -1,11 +1,62 @@
 import React from "react"
+import { useParams } from "react-router-dom"
+import { ArrowUpRight, Globe } from "lucide-react"
 
+import { projects } from "../data/projects"
 import styles from "./ProjectPage.module.css"
 
 function ProjectPage() {
+    const { title } = useParams()
+    const project = projects.find(
+        (p) => p.title.toLowerCase() === title.toLowerCase(),
+    )
+
+    if (!project) {
+        return <div className={styles.notFound}>Project not found</div>
+    }
+
     return (
-        <div className={styles.tempDiv}>
-            <h1 className={styles.temp}>In Process!</h1>
+        <div className={styles.projectPage}>
+            <div className={styles.projectHeader}>
+                <img
+                    src={project.img}
+                    alt={project.title}
+                    className={styles.projectImage}
+                />
+                <div className={styles.projectInfo}>
+                    <h1 className={styles.projectTitle}>{project.title}</h1>
+                    <p className={styles.projectDescription}>{project.des}</p>
+                    <hr className={styles.divider} />
+                    <div className={styles.iconList}>
+                        {project.iconLists.map((Icon) => (
+                            <Icon size={24} />
+                        ))}
+                    </div>
+                    <div className={styles.buttonContainer}>
+                        <a
+                            href={project.link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className={styles.button}
+                        >
+                            LIVE <ArrowUpRight size={24} />
+                        </a>
+                        <a
+                            href={project.github}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className={styles.buttonGithub}
+                        >
+                            GITHUB <Globe size={22} />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <hr className={styles.divider} />
+            <div className={styles.projectContent}>
+                <p className={styles.paragraph}>{project.paragraph1}</p>
+                <p className={styles.paragraph}>{project.paragraph2}</p>
+            </div>
         </div>
     )
 }
--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -25,25 +25,23 @@ export const projects = [
         title: "Stox",
         des: "Tokenizing Stocks Using Chainlink",
         img: StoX,
-        iconLists: [
-            SolidityOriginal,
-            Chainlink,
-            JavascriptOriginal,
-            TailwindcssOriginal,
-        ],
+        img2: "",
+        iconLists: [SolidityOriginal, JavascriptOriginal, TailwindcssOriginal],
         link: "https://stox-tokenize.vercel.app",
         github: "https://github.com/eik-1/stox-foundry-contracts",
-        paragraph1: "",
-        paragraph2: "",
+        paragraph1:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        paragraph2:
+            "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
     },
     {
         id: 2,
         title: "D Beats",
         des: "NFT Marketplace For Music",
         img: dBeats,
+        img2: "",
         iconLists: [
             SolidityOriginal,
-            Chainlink,
             HardhatOriginal,
             JavascriptOriginal,
             ReactOriginal,
